1cc64285c62a995096597c66e3d47c2402c50b57
egoist945402376 <egoist945402376@gmail.com>
2025-03-10 11:22:29 +0000

    Buyer and farmer registration page.

--- tropiconnect/supplychain/urls.py
+++ tropiconnect/supplychain/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/farmer/', views.register_farmer, name='register_farmer'),
+    path('register/buyer/', views.register_buyer, name='register_buyer'),
 ]
--- tropiconnect/supplychain/views.py
+++ tropiconnect/supplychain/views.py
@@ -7,3 +7,19 @@ def home(request):
     return render(request, 'supplychain/home.html', {
         'title': 'TropiConnect Homg page'
     })
+
+def register_farmer(request):
+    """
+    View function for farmer registration (UI only)
+    """
+    return render(request, 'supplychain/register_farmer.html', {
+        'title': 'Farmer Registration'
+    })
+
+def register_buyer(request):
+    """
+    View function for buyer registration (UI only)
+    """
+    return render(request, 'supplychain/register_buyer.html', {
+        'title': 'Buyer Registration'
+    })
